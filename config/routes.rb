0145ea724fd65e0285ea8696e30c960bbe9a2926
Harry Brown <harry@designerpages.com>
2015-04-21 19:01:23 -0500

    slider functional and creating footer menu

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Career::Application.routes.draw do
 
   resources :users
 
+  resources :about
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
