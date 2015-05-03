8ae681cdd0ba7f605761e4cc26eb25d398778da7
Leroy Brown <harry@-rexdesigns.com>
2015-05-03 15:33:11 -0500

    wip

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,11 +1,5 @@
-# config/initializers/pdfkit.rb
 PDFKit.configure do |config|
-  config.wkhtmltopdf = '/path/to/wkhtmltopdf'
-  config.default_options = {
-    :page_size => 'Legal',
-    :print_media_type => true
-  }
-  # Use only if your external hostname is unavailable on the server.
-  config.root_url = "http://localhost"
-  config.verbose = false
+  if File.executable? '/app/.apt/usr/local/bin/wkhtmltopdf'
+    config.wkhtmltopdf = '/app/.apt/usr/local/bin/wkhtmltopdf'
+  end
 end
